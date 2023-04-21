086429fdf5bf34a9af03af7fb92fabdab5db23e4
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-04-21 14:35:19 -0400

    Update nicolay_1.py

--- nicolay_1.py
+++ nicolay_1.py
@@ -148,7 +148,7 @@ if submit_button_1:
                     #st.text(line)
 
                 for line in text_lines:
-                    st.markdown(line)
+                    st.write(line)
 
 
 

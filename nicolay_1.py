250298129d0cadcee3eaf4fb38bd9a268de7adef
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-04-25 11:30:08 -0400

    Update nicolay_1.py

--- nicolay_1.py
+++ nicolay_1.py
@@ -51,7 +51,7 @@ os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
 openai.api_key = os.getenv("OPENAI_API_KEY")
 #os.environ["SERPAPI_API_KEY"] = st.secrets["serpapi_api_key"]
 
-st.title("Nicolay: Exploring the Speeches of Abraham Lincoln with AI (version 0.1)")
+st.title("Nicolay: Exploring the Speeches of Abraham Lincoln with AI")
 
 
 st.write("This application uses OpenAI's GPT AI models to answer questions about the collected speeches of Abraham Lincoln. Choose one of the options below, and pose a question about the text.")

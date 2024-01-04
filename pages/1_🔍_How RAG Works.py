df20c824a571fea523dc61edece156fb3e2afa12
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-01-03 22:57:30 -0500

    Update 1_🔍_How RAG Works.py

--- pages/1_🔍_How RAG Works.py
+++ pages/1_🔍_How RAG Works.py
@@ -60,7 +60,18 @@ keyword_prompt = st.session_state['keyword_model_system_prompt']
 response_prompt = st.session_state['response_model_system_prompt']
 
 # Streamlit interface
-st.title("Search the Lincoln Corpus with RAG")
+st.title("Exploring RAG with Nicolay and Hay")
+
+image_url = 'http://danielhutchinson.org/wp-content/uploads/2024/01/nicolay_hay.png'
+st.markdown(f'<img src="{image_url}" width="700">', unsafe_allow_html=True)
+
+
+
+
+
+
+
+
 
 # Query input
 with st.form("Search Interface"):

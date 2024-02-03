a6be009b2ab2302d164637a23506ecc9219a9d73
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-02-02 21:33:20 -0500

    Update 🏛️_Nicolay_0.2.py

--- 🏛️_Nicolay_0.2.py
+++ 🏛️_Nicolay_0.2.py
@@ -55,7 +55,8 @@ with col2:
     # ORIGINAL DON'T DELETE
 
     image_url = 'http://danielhutchinson.org/wp-content/uploads/2023/05/title_card.png'
-    st.markdown(f'<div style="text-align: center;"><img src="{image_url}" width="700" style="margin: 0 auto;"></div>', unsafe_allow_html=True)
+    st.markdown(f'<div style="text-align: center;"><img src="{image_url}" width="350"></div>', unsafe_allow_html=True)
+
 
 
 with col3:

1be801350f652ca480c69797ab52cf0360335509
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-02-02 22:08:22 -0500

    Update 🏛️_Nicolay_0.2.py

--- 🏛️_Nicolay_0.2.py
+++ 🏛️_Nicolay_0.2.py
@@ -87,6 +87,8 @@ instruction = """
 
 st.markdown(instruction, unsafe_allow_html=True)
 
+st.write("\n\n")
+
 st.write("**Exploring RAG with Hay and Nicolay:** This section explores how Retrieval Augmented Generation enables large language models to explore extensive text collections. Users can gain familarity with the mechanics of RAG via an interface for conducting their own searches over the Lincoln speech corpus using two finetuned LLMs, Hay and Nicolay.")
 
 st.write("**Ask Nicolay:** (Coming Winter 2024) This interactive feature invites users to engage in a dynamic dialogue with Lincoln’s speeches. Powered by a sophisticated RAG-based chatbot, this section offers an unprecedented opportunity to ask open-ended questions and receive responses showcasing Lincoln's historic language.")

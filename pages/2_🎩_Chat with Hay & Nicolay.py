033a80462ebd89c882d89b16bbb4a55024d6159c
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-05-20 21:29:23 -0400

    Update 2_🎩_Chat with Hay & Nicolay.py

--- pages/2_🎩_Chat with Hay & Nicolay.py
+++ pages/2_🎩_Chat with Hay & Nicolay.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from modules.rag_process import RAGProcess
-from modules.data_logging import DataLogger, log_keyword_search_results, log_semantic_search_results, log_reranking_results, log_nicolay_model_output
-import json
-import os
-from openai import OpenAI
+import openai
 import cohere
 import pygsheets
 from google.oauth2 import service_account
 from llama_index.llms.openai import OpenAI as LlamaOpenAI
-from llama_index import VectorStoreIndex, ServiceContext, SimpleDirectoryReader
+from llama_index import VectorStoreIndex, ServiceContext
+from llama_index.readers.file import SimpleDirectoryReader
+from modules.rag_process import RAGProcess
+from modules.data_logging import DataLogger, log_keyword_search_results, log_semantic_search_results, log_reranking_results, log_nicolay_model_output
+
 
 # Configure Streamlit
 st.set_page_config(page_title="Nicolay: Exploring the Speeches of Abraham Lincoln with AI (version 0.2)", layout='wide', page_icon='🎩')

0c9a1e3073c0d8a6dd697a1d9d6cd52afbab79a7
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-05-22 15:56:46 -0400

    init

--- modules/data_utils.py
+++ modules/data_utils.py
@@ -1,40 +1,18 @@
-import msgpack
-import json
+# data_utils.py
 import streamlit as st
-from concurrent.futures import ThreadPoolExecutor
+import json
 import pandas as pd
-import pyarrow as pa
-import pyarrow.parquet as pq
 
-
-# Load data functions with caching
 @st.cache_data(persist="disk")
 def load_lincoln_speech_corpus():
-    with open('data/lincoln_speech_corpus.msgpack', 'rb') as file:
-        return msgpack.unpackb(file.read())
+    with open('data/lincoln_speech_corpus.json', 'r') as file:
+        return json.load(file)
 
 @st.cache_data(persist="disk")
 def load_voyant_word_counts():
-    with open('data/voyant_word_counts.msgpack', 'rb') as file:
-        return msgpack.unpackb(file.read())
+    with open('data/voyant_word_counts.json', 'r') as file:
+        return json.load(file)
 
 @st.cache_data(persist="disk")
 def load_lincoln_index_embedded():
-    df = pd.read_parquet('data/lincoln_index_embedded.parquet')
-    # Ensure the index is correctly set
-    df.reset_index(inplace=True)
-    df.rename(columns={'index': 'text_id'}, inplace=True)
-    return df
-
-@st.cache_data(persist="disk")
-def load_all_data():
-    with ThreadPoolExecutor() as executor:
-        lincoln_future = executor.submit(load_lincoln_speech_corpus)
-        voyant_future = executor.submit(load_voyant_word_counts)
-        index_future = executor.submit(load_lincoln_index_embedded)
-
-        lincoln_data = lincoln_future.result()
-        voyant_data = voyant_future.result()
-        index_data = index_future.result()
-
-    return lincoln_data, voyant_data, index_data
+    return pd.read_csv('lincoln_index_embedded.csv')

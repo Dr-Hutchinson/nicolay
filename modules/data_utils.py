a37dc789d9a9430172317e5a6d7e558b5fe1c60f
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-05-22 14:51:20 -0400

    Update data_utils.py

--- modules/data_utils.py
+++ modules/data_utils.py
@@ -3,16 +3,16 @@ import streamlit as st
 import json
 import pandas as pd
 
-@st.cache_data(persist="disk", ttl=3600)
+@st.cache_data(persist="disk")
 def load_lincoln_speech_corpus():
     with open('data/lincoln_speech_corpus.json', 'r') as file:
         return json.load(file)
 
-@st.cache_data(persist="disk", ttl=3600)
+@st.cache_data(persist="disk")
 def load_voyant_word_counts():
     with open('data/voyant_word_counts.json', 'r') as file:
         return json.load(file)
 
-@st.cache_data(persist="disk", ttl=3600)
+@st.cache_data(persist="disk")
 def load_lincoln_index_embedded():
     return pd.read_csv('lincoln_index_embedded.csv')

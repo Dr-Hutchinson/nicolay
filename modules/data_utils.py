87f62545f14f5e301e53af3de5053fdb877c816c
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-05-23 14:18:36 -0400

    Update data_utils.py

--- modules/data_utils.py
+++ modules/data_utils.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 @st.cache_data(persist="disk")
 def load_lincoln_speech_corpus():
-    with open('/mnt/data/lincoln_speech_corpus.msgpack', 'rb') as f:
+    with open('data/lincoln_speech_corpus.msgpack', 'rb') as f:
         unpacker = msgpack.Unpacker(f, raw=False)
         data = [unpacked for unpacked in unpacker]
 
@@ -25,11 +25,11 @@ def load_lincoln_speech_corpus():
 
 @st.cache_data(persist="disk")
 def load_voyant_word_counts():
-    with open('/mnt/data/voyant_word_counts.msgpack', 'rb') as f:
+    with open('data/voyant_word_counts.msgpack', 'rb') as f:
         unpacker = msgpack.Unpacker(f, raw=False)
         data = [unpacked for unpacked in unpacker]
         return data[0]
 
 @st.cache_data(persist="disk")
 def load_lincoln_index_embedded():
-    return pd.read_parquet('/mnt/data/lincoln_index_embedded.parquet')
+    return pd.read_parquet('data/lincoln_index_embedded.parquet')

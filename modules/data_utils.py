f727b6b363cc2915a322b9e918d32953704c18c4
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2024-05-23 19:24:55 -0400

    Update data_utils.py

--- modules/data_utils.py
+++ modules/data_utils.py
@@ -9,10 +9,6 @@ def load_lincoln_speech_corpus():
         unpacker = msgpack.Unpacker(f, raw=False)
         data = [unpacked for unpacked in unpacker][0]
         df = pd.DataFrame(data)
-        st.write("Lincoln Speech Corpus DataFrame Head:")
-        st.write(df.head())
-        st.write("Lincoln Speech Corpus DataFrame Info:")
-        st.write(df.info())
         return df
 
 @st.cache_data(persist="disk")
@@ -21,10 +17,6 @@ def load_voyant_word_counts():
         unpacker = msgpack.Unpacker(f, raw=False)
         data = [unpacked for unpacked in unpacker][0]
         df = pd.DataFrame([data])
-        st.write("Voyant Word Counts DataFrame Head:")
-        st.write(df.head())
-        st.write("Voyant Word Counts DataFrame Info:")
-        st.write(df.info())
         # Correctly parse 'corpusTerms' JSON string
         df['corpusTerms'] = df['corpusTerms'].apply(lambda x: json.loads(x) if isinstance(x, str) else x)
         return df
@@ -32,12 +24,6 @@ def load_voyant_word_counts():
 @st.cache_data(persist="disk")
 def load_lincoln_index_embedded():
     df = pd.read_parquet('data/lincoln_index_embedded.parquet')
-    st.write("Lincoln Index Embedded DataFrame Head:")
-    st.write(df.head())
-    st.write("Lincoln Index Embedded DataFrame Info:")
-    st.write(df.info())
     if 'combined' in df.columns:
         df['text_id'] = df['combined'].str.extract(r'(Text #: \d+)')
-    else:
-        st.write("Error: 'combined' column not found in the DataFrame")
     return df

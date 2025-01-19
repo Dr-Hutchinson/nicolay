e62d66b69888d18a5e4b1e4b84213e4d0c5dfb43
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2025-01-19 17:35:14 -0500

    Update rag_pipeline.py

--- modules/rag_pipeline.py
+++ modules/rag_pipeline.py
@@ -533,5 +533,5 @@ def run_rag_pipeline(
             "search_results": search_results_df,
             "semantic_results": semantic_matches_df,
             "reranked_results": reranked_df,
-            "nicolay_output": nicolay_output,
+            "nicolay_output": nicolay_output
         }

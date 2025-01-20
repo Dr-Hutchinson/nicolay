63e7dfddc1f6c7645c746825ec5777b6ba61ecc7
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2025-01-20 15:03:13 -0500

    Update rag_evaluator.py

--- modules/rag_evaluator.py
+++ modules/rag_evaluator.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.translate.bleu_score import sentence_bleu
 from rouge_score import rouge_scorer
 import pandas as pd
+import streamlit as st
 
 class RAGEvaluator:
     def __init__(self):
